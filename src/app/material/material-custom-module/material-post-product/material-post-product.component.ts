e0c65df40d52953977b3515353ddcab86b67707a
vvaldesc <victor.valdescobos@riberadeltajo.es>
2024-03-23 12:36:02 +0100

    formulario post products campos generados dinámicamente

--- src/app/material/material-custom-module/material-post-product/material-post-product.component.ts
+++ src/app/material/material-custom-module/material-post-product/material-post-product.component.ts
@@ -12,12 +12,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {CommonModule} from '@angular/common';
 
 import {Product} from '../../../models/Product';
 
 export interface DialogData {
-  animal: string;
-  name: string;
+  productInterfaceKeys: string[];
+  productInterface: Product;
 }
 
 /**
@@ -32,19 +33,28 @@ export interface DialogData {
 export class MaterialPostProductComponent {
   animal: string = '';
   name: string = '';
-
-
+  product: Product = {
+    image: '',
+    name: '',
+    description: '',
+    unitAvailable: 0,
+    date: new Date(),
+    price: 0,
+    category: '',
+  };
+  keys: string[] = Object.keys(this.product);
 
   constructor(public dialog: MatDialog) {}
 
   openDialog(): void {
+    console.log(this.keys);
     const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
-      data: {name: this.name, animal: this.animal},
+      data: {productInterfaceKeys: this.keys},
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
-      this.animal = result;
+      this.product = result;
     });
   }
 }
@@ -62,6 +72,7 @@ export class MaterialPostProductComponent {
     MatDialogContent,
     MatDialogActions,
     MatDialogClose,
+    CommonModule
   ],
 })
 export class DialogOverviewExampleDialog {
